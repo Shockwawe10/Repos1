dc6d352b8c8372d05493abed95008c1de2b6a4c7
Shockwawe10 <83669863+Shockwawe10@users.noreply.github.com>
2021-05-06 12:42:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ th{
     </style>
             
         <table class="nav">
-            <td><a href="films.php">Рєєстрація фільму</a></td>
+            <td><a href="films.php">Рєєстрація</a></td>
             <td><a href="index.php">Записи</a></td>
         </table>
       
